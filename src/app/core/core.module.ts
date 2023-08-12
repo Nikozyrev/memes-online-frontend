774ad24345385699bc71b1abbaa0dbe918fd349a
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-08-12 13:44:06 +0300

    config: add core module and ngrx modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 })
 export class CoreModule {}
