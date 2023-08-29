1c9558b705805f72ca71fd977b33b479e74e5a1e
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2023-08-29 17:40:56 +0300

    refactor: effects operators optimization

--- src/app/game/store/effects/player-memes.effects.ts
+++ src/app/game/store/effects/player-memes.effects.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { mergeMap, of } from 'rxjs';
+import { of, switchMap } from 'rxjs';
 import { playerMemesActions } from '../actions/player-memes.actions';
 import { PlayerMemesService } from '../../services/player-memes/player-memes.service';
 
@@ -9,7 +9,7 @@ export class PlayerMemesEffects {
   getMemes$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(playerMemesActions.getMemes),
-      mergeMap(() =>
+      switchMap(() =>
         of(
           playerMemesActions.getMemesSuccess({
             memes: this.playerMemesService.getMemes(),
