af5415900e6b903fb2c9a0eb45bd94e120d4a091
Nikita Kozyrev <n.i.kozyrev@gmail.com>
2024-01-06 16:43:28 +0300

    refactor: change state to signals

--- src/app/game/components/game-info/game-info.component.ts
+++ src/app/game/components/game-info/game-info.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Signal, computed } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   selectGameInfoState,
   selectIsUserActive,
 } from '../../store/selectors/game-info.selectors';
 import { selectError } from '../../store/selectors/game-status.selectors';
+import { IGameInfoState } from '../../store/models/game-info.model';
 
 @Component({
   selector: 'app-game-info',
@@ -12,9 +13,17 @@ import { selectError } from '../../store/selectors/game-status.selectors';
   styleUrls: ['./game-info.component.scss'],
 })
 export class GameInfoComponent {
-  gameInfo = this.store.select(selectGameInfoState);
-  error = this.store.select(selectError);
-  isUserActive = this.store.select(selectIsUserActive);
+  public gameInfo: Signal<IGameInfoState>;
+  public error: Signal<string | null>;
+  public isUserActive: Signal<boolean>;
 
-  constructor(private store: Store) {}
+  public activeUser: Signal<string> = computed(() =>
+    this.isUserActive() ? 'You' : this.gameInfo().activeUser?.name || 'Unknown'
+  );
+
+  constructor(private store: Store) {
+    this.gameInfo = this.store.selectSignal(selectGameInfoState);
+    this.error = this.store.selectSignal(selectError);
+    this.isUserActive = this.store.selectSignal(selectIsUserActive);
+  }
 }
--- src/app/game/components/game/game.component.ts
+++ src/app/game/components/game/game.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Signal, computed } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { combineLatest } from 'rxjs';
 import {
   selectIsUserActive,
   selectStage,
 } from '../../store/selectors/game-info.selectors';
 import { selectIsSituationSelected } from '../../store/selectors/situations.selectors';
+import { Stage } from '../../models/game-info.model';
 
 @Component({
   selector: 'app-game',
@@ -13,11 +13,32 @@ import { selectIsSituationSelected } from '../../store/selectors/situations.sele
   styleUrls: ['./game.component.scss'],
 })
 export class GameComponent {
-  state = combineLatest({
-    currentStage: this.store.select(selectStage),
-    isSituationSelected: this.store.select(selectIsSituationSelected),
-    isUserActive: this.store.select(selectIsUserActive),
-  });
+  public currentStage: Signal<Stage | null>;
 
-  constructor(private store: Store) {}
+  public isSituationSelected: Signal<boolean>;
+
+  public isUserActive: Signal<boolean>;
+
+  public canSelectSituation = computed(
+    () =>
+      this.currentStage() === '1' &&
+      this.isUserActive() &&
+      !this.isSituationSelected()
+  );
+
+  public canShowWinner = computed(
+    () => this.currentStage() === '3' || this.currentStage() === '4'
+  );
+
+  public canSelectMeme = computed(
+    () => this.currentStage() === '2' && !this.isUserActive()
+  );
+
+  constructor(private store: Store) {
+    this.currentStage = this.store.selectSignal(selectStage);
+    this.isSituationSelected = this.store.selectSignal(
+      selectIsSituationSelected
+    );
+    this.isUserActive = this.store.selectSignal(selectIsUserActive);
+  }
 }
